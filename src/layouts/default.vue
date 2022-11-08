0d0cf2043d101e785054c28bc0be62a88ccfc98d
Dmitry <60594670+YNDmitry@users.noreply.github.com>
2022-11-09 02:09:51 +0500

    delete nuxt indication

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
 	<main class="main">
-		<NuxtLoadingIndicator></NuxtLoadingIndicator>
 		<AppPreloader></AppPreloader>
 		<AppTransition></AppTransition>
 		<DefaultBanner></DefaultBanner>
