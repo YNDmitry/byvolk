34ad7013f9ad91227b5630d3b6ceeaf4a53f6e16
Dmitry <60594670+YNDmitry@users.noreply.github.com>
2022-11-25 18:38:33 +0500

    new blocks

---
+++ src/storyblok/richText.vue
@@ -0,0 +1,18 @@
+<template>
+  <section v-editable="blok" class="rich">
+    <div class="container">
+      <div v-html="richtext" v-if="props.blok.richtext.content.length > 0"></div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+  const props = defineProps({
+		blok: {
+			type: Object,
+			default: () => ({}),
+		},
+	})
+
+	const richtext = computed(() => renderRichText(props.blok.description))
+</script>
