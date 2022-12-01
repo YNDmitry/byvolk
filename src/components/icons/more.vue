744c418af8606527bb6b16cf565d6674a73f94d6
Dmitry <60594670+YNDmitry@users.noreply.github.com>
2022-12-02 03:02:27 +0500

    style fixes

---
+++ src/components/icons/more.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg width="40" height="30" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M13 12a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"></path>
+    <path d="M9 12a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"></path>
+    <path d="M17 12a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"></path>
+  </svg>
+</template>
--- src/storyblok/instagramBlock.vue
+++ src/storyblok/instagramBlock.vue
@@ -7,21 +7,40 @@
 				:class="blok.headline.length === 0 ? '' : 'mt-medium'"
 			>
 				<div 
-					v-for="item in blok.instagramImages"
+					v-for="item in blok.cards"
 					:key="item"
-					class="instagram__photos-item"
+					class="instagram__photos-item up"
 				>
+					<div class="instagram__photos-item-head">
+						<div class="instagram__photos-item-info">
+							<div class="instagram__photos-item-avatar">
+								<img :src="item.avatar.filename" alt="" v-if="item.avatar">
+							</div>
+							<div class="instagram__photos-item-user">
+								<div v-if="item.name">{{ item.name }}</div>
+								<div v-if="item.nickname">{{ item.nickname }}</div>
+							</div>
+						</div>
+						<IconsMore class="instagram__photos-item-head-icon"></IconsMore>
+					</div>
 					<NuxtLink
-						class="up"
 						to="https://www.instagram.com/byvolk/" 
 						target="_blank"
 					>
 						<img
-							:src="item?.filename + '/m/filters:quality(10)'"
+							:src="item?.image.filename + '/m/filters:quality(10)'"
 							width="350"
 							loading="lazy"
+							v-if="item?.image"
 						/>
 					</NuxtLink>
+					<div class="instagram__photos-item-footer" v-if="item.likes">
+						<div class="instagram__photos-item-footer-likes">
+							<IconsHearh class="instagram__photos-item-footer-likes-icon"></IconsHearh>
+							<span>{{ item.likes }}</span>
+						</div>
+						<IconsShareForward width="20" height="20"></IconsShareForward>
+					</div>
 				</div>
 			</div>
 		</div>
