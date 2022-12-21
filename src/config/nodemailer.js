db3828c11d4d995d4e8cc89713054219ebd96fef
Dmitry <60594670+YNDmitry@users.noreply.github.com>
2022-12-21 17:25:58 +0500

    mail settings

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -2,8 +2,7 @@ import nodemailer from 'nodemailer'
 
 export const transporter = nodemailer.createTransport({
   host: 'mail.privateemail.com',
-  port: 465,
-  secure: true,
+  port: 587,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
@@ -14,3 +13,11 @@ export const mailOptions = {
   from: `<${process.env.EMAIL}>`,
   to: process.env.EMAIL
 }
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
