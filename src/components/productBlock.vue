5d26e623850340e087ad54283d3780f92a33d160
Dmitry <60594670+YNDmitry@users.noreply.github.com>
2023-02-20 00:12:43 +0500

    refactoring

--- src/components/productBlock.vue
+++ src/components/productBlock.vue
@@ -3,10 +3,10 @@
 		<div class="product-card__head" v-if="images[0].node.src">
 			<NuxtImg
 				:src="images[0].node.src"
-				:srcset="`${images[0].node.src}&width=320 320w, ${images[0].node.src}&width=640 640w, ${images[0].node.src}&width=768 768w, ${images[0].node.src}&width=1024 1024w, ${images[0].node.src}&width=1280 1280w, ${images[0].node.src}&width=1536 1536w, ${images[0].node.src}&width=1536 1536w`"
+				:srcset="`${images[0].node.src}&width=320 320w, ${images[0].node.src}&width=640 640w`"
 				:alt="title"
-				sizes="(max-width: 320px) 100vw, (max-width: 640px) 100vw, (max-width: 768px) 100vw, (max-width: 1024px) 100vw, (max-width: 1280px) 100vw, (max-width: 1536px) 100vw, 100vw"
-				width="300"
+				sizes="(max-width: 320px) 100vw, (max-width: 640px) 100vw"
+				width="180"
 				loading="lazy"
 			></NuxtImg>
 		</div>
