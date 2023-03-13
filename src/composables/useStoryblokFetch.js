bd8a1ce5b92cf525fb2d031d22af421ed525cffc
Dmitry <60594670+YNDmitry@users.noreply.github.com>
2023-03-13 17:50:27 +0500

    storyblok fixes

--- src/composables/useStoryblokFetch.js
+++ src/composables/useStoryblokFetch.js
@@ -1,5 +1,5 @@
 export const useStoryblokFetch = async (locale) => {
-	const config = useRuntimeConfig()
+	const version = useRuntimeConfig().public.storyblokVersion
 	const route = useRoute();
 	let currentRoute = { ...route }
 
@@ -17,17 +17,14 @@ export const useStoryblokFetch = async (locale) => {
 
 	let story
 
-
 	if (route.query['_storyblok_tk[token]'] !== undefined) {
 		story = await useAsyncStoryblok(currentRoute.path, {
-			version: 'draft',
-			token: config.public.storyblokPreview
+			version: version,
 		})
 	} else {
 		story = await useAsyncData(`${currentRoute.path}-asyncdata`, async () =>
 			await storyblokApiInstance.get(`cdn/stories${currentRoute.path}`, {
-				version: 'published',
-				token: config.public.storyblokPublic
+				version: version,
 			})).then((res) => res?.data?.value?.data?.story)
 	}
 
